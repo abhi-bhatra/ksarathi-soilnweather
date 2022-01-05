8370b1c4b126c617dc5848ae6ba59677b27a3ce1
= <=>
2022-01-05 12:24:56 +0530

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Soil from './Components/Soil';
 import Weather from './Components/Weather';
 
@@ -8,11 +8,10 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <h2>Welcome to Router</h2>
           <nav className='navbar navbar-expand-lg navbar-light bg-light'>
           <ul className="navbar-nav mr-auto">
-            <li><Link to={'/Soil'} className="nav-link">Soil</Link></li>
-            <li><Link to={'/Weather'} className="nav-link">Weather Forecast</Link></li>
+            {/* <li><Link to={'/Soil'} className="nav-link">Soil</Link></li>
+            <li><Link to={'/Weather'} className="nav-link">Weather Forecast</Link></li> */}
           </ul>
           </nav>
           <hr />
